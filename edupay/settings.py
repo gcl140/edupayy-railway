1588fbb46d1f34b9d1e8429ce5161ad12f9cfc9c
Gift Christian <140011591+gcl140@users.noreply.github.com>
2025-06-05 19:16:08 +0300

    shekh

--- edupay/settings.py
+++ edupay/settings.py
@@ -76,15 +76,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'edupay.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+# # Database
+# # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
@@ -186,8 +186,11 @@ CELERY_TASK_SERIALIZER = 'json'
 INSTALLED_APPS += ['django_celery_beat']
 
 
-
 import dj_database_url
 
-DATABASES['default'] = dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'), conn_max_age=600)
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
+}
